b2f84429b03ddcd82aa59aa88ab295c8b2499c86
Blaise Liu <blaise.liu@egov.com>
2020-11-21 01:32:40 -0500

    ResultFilterAttribute

---
+++ Reference.DotNetCore.AsyncApi/Reference.DotNetCore.AsyncApi/Filters/NoteResultFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Reference.DotNetCore.AsyncApi.Filters
+{
+    public class NoteResultFilterAttribute:ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var resultFromAction = context.Result as ObjectResult;
+            if (resultFromAction?.Value==null ||
+                resultFromAction.StatusCode<200 ||
+                resultFromAction.StatusCode>=300)
+            {
+                await next();
+                return;
+            }
+            
+            await next();
+        }
+    }
+}
