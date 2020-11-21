b71a1b8d20c91afe5f32d50fd8ceb096eea651e6
Blaise Liu <blaise.liu@egov.com>
2020-11-21 00:16:34 -0500

    DbContext

---
+++ Reference.DotNetCore.AsyncApi/Reference.DotNetCore.AsyncApi/Context/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reference.DotNetCore.AsyncApi.Entities;
+
+namespace Reference.DotNetCore.AsyncApi.Context
+{
+    public class NotesDbContext: DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
