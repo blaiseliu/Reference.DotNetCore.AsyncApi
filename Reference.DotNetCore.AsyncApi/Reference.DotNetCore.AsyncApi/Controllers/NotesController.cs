cd482270874f5a596669ce8d4c436d17902d5c13
Blaise Liu <blaise.liu@egov.com>
2020-11-21 01:26:22 -0500

    async ApiController

--- Reference.DotNetCore.AsyncApi/Reference.DotNetCore.AsyncApi/Controllers/NotesController.cs
+++ Reference.DotNetCore.AsyncApi/Reference.DotNetCore.AsyncApi/Controllers/NotesController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Reference.DotNetCore.AsyncApi.Repositories;
 
 namespace Reference.DotNetCore.AsyncApi.Controllers
 {
@@ -9,18 +11,28 @@ namespace Reference.DotNetCore.AsyncApi.Controllers
     public class NotesController:ControllerBase
     {
         private readonly ILogger<NotesController> _logger;
+        private readonly INoteRepository _repository;
 
-        
-
-        public NotesController(ILogger<NotesController> logger)
+        public NotesController(ILogger<NotesController> logger, INoteRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("")]
+        public async Task<IActionResult> Get()
         {
-            return null;
+            var entities=await _repository.GetAllAsync();
+            return Ok(entities);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var entity= await _repository.FindAsync(id);
+            if (entity == null) return NotFound();
+            return Ok(entity);
         }
     }
 }
