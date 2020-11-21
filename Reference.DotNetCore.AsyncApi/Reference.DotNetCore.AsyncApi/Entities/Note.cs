0e18c36dc6d1cf0076a810e7142954fc85a86261
Blaise Liu <blaise.liu@egov.com>
2020-11-21 00:13:22 -0500

    Entity model: Note

---
+++ Reference.DotNetCore.AsyncApi/Reference.DotNetCore.AsyncApi/Entities/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reference.DotNetCore.AsyncApi.Entities
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required] 
+        public DateTime DateCreated { get; set; }
+        [Required] 
+        public DateTime DateLastModified{ get; set; }
+    }
+}
